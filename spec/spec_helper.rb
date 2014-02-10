c17b100af65c0c7a31f93c6384d69c44fcada7ab
Peter Souter <p.morsou@gmail.com>
2014-02-10 15:41:17 +0000

    Add VCR settings to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,27 @@ end
 require 'dropbox-api'
 require 'rspec'
 require 'webmock/rspec'
+require 'vcr'
 
 module Dropbox
   Spec = Hashie::Mash.new
 end
 
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr/cassettes'
+  c.hook_into :webmock
+
+  c.filter_sensitive_data('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') do |interaction|
+    interaction.request.headers['Authorization'].first
+  end
+end
+
+
 Dir.glob("#{File.dirname(__FILE__)}/support/*.rb").each { |f| require f }
 
 # Clean up after specs, remove test-directory
 RSpec.configure do |config|
+  config.extend VCR::RSpec::Macros
   config.after(:all) do
     test_dir = Dropbox::Spec.instance.find(Dropbox::Spec.test_dir) rescue nil
     test_dir.destroy if test_dir and !test_dir.is_deleted?
