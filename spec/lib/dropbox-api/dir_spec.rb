66969bbe60011cf7183103630b07058911d27086
Peter Souter <p.morsou@gmail.com>
2014-02-10 21:09:21 +0000

    All specs changed to use VCR I created a dummy user that I can delete afterwards just in case I leak any data, but I've added the authorisation to the filtered config so if they're re-recorded in the future they'll be no issues

--- spec/lib/dropbox-api/dir_spec.rb
+++ spec/lib/dropbox-api/dir_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe Dropbox::API::Dir do
+describe Dropbox::API::Dir, vcr: true do
 
   after do
     # @dir.delete
@@ -52,6 +52,16 @@ describe Dropbox::API::Dir do
         @dir.is_deleted.should == true
       end
 
+    end
+
+  end
+
+  describe "#direct_url" do
+
+    it "gives the direct url" do
+      direct_url_object = @dir.direct_url
+      direct_url_object.url.should eql 'https://www.dropbox.com/sh/y2wx4tpk9co1uqg/7wFKjUC2ep'
+      direct_url_object.expires.should eql 'Tue, 01 Jan 2030 00:00:00 +0000'
     end
 
   end
